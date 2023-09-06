07182add90f39792b4b006dfde444b6b439d618c
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-09-06 19:52:45 +0530

    Update config.py

--- helper/config.py
+++ helper/config.py
@@ -15,7 +15,7 @@
 from . import *
 
 try:
-    APP_ID = config("", default=12278858 cast=int)
+    APP_ID = config("", default=12278858, cast=int)
     API_HASH = config("ce109d0baed40d7da8a762be8737da4f")
     BOT_TOKEN = config("5696982423:AAHZ1XwGXCwVxjtMobLfaGnZUzykNl5_0_s")
     OWNER = config("", default=5591954930, cast=int)
